5bf0350d3d5e9acd5c0182440adeb3bba7a29604
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-10-06 14:35:44 +0530

    remove require(lspconfig)

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,46 +1,26 @@
--- load defaults i.e lua_lsp
+-- -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
-
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+local servers = { "html", "cssls", "nil_ls", "gopls", "clangd" }
+vim.lsp.enable(servers)
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+-- override server-specific config:
 
--- LSP nix setup
-lspconfig.nil_ls.setup {
+vim.lsp.config("nil_ls", {
   autostart = true,
   on_attach = nvlsp.on_attach,
   capabilities = nvlsp.capabilities,
   settings = {
     ["nil"] = {
-      formatting = {
-        command = { "nixfmt" }, -- Ensure you have nixpkgs-fmt installed
-      },
+      formatting = { command = { "nixfmt" } },
       single_file_support = true,
       autoArchive = true,
     },
   },
-}
+})
 
--- LSP gopls setup
-lspconfig.gopls.setup {
+vim.lsp.config("gopls", {
   on_attach = nvlsp.on_attach,
   capabilities = nvlsp.capabilities,
   settings = {
@@ -54,40 +34,33 @@ lspconfig.gopls.setup {
         parameterNames = true,
         rangeVariableTypes = true,
       },
-      analyses = {
-        unusedparams = true,
-      },
+      analyses = { unusedparams = true },
       staticcheck = true,
       gofumpt = true,
       usePlaceholders = true,
-      completeUnimported = true, -- Enables auto-importing
+      completeUnimported = true,
       deepCompletion = true,
       directoryFilters = { "-node_modules" },
     },
   },
-}
+})
 
--- Autoformat and organize imports on save
+vim.lsp.config("clangd", {
+  on_attach = function(client, bufnr)
+    if nvlsp.on_attach then
+      nvlsp.on_attach(client, bufnr)
+    end
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true)
+    end
+  end,
+  capabilities = nvlsp.capabilities,
+})
+
+-- For formatting on save:
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
     vim.lsp.buf.format { async = false }
   end,
 })
-
--- LSP clangd setup
-lspconfig.clangd.setup {
-  on_init = nvlsp.on_init,
-  on_attach = function(client, bufnr)
-    -- Call the default NvChad on_attach
-    if nvlsp.on_attach then
-      nvlsp.on_attach(client, bufnr)
-    end
-
-    -- Enable inlay hints if the LSP supports it
-    if client.server_capabilities.inlayHintProvider then
-      vim.lsp.inlay_hint.enable(true)
-    end
-  end,
-  capabilities = nvlsp.capabilities,
-}
